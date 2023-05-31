4d4db40a22c1991e062cf5c2cb5a28c7f27a379a
Sergej Grilborzer <sergej@grilborzer.de>
2023-05-31 10:42:05 +0200

    Add first unit test example

--- src/test/java/de/neuefische/shopsystem/service/ShopServiceTest.java
+++ src/test/java/de/neuefische/shopsystem/service/ShopServiceTest.java
@@ -12,19 +12,36 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class ShopServiceTest {
 
+    // mock = Dummy = Gaukelt vor es gäbe ein Objekt von der Klasse
+    // Wir nutzen das, um NUR die Zielklasse zu testen (also den ShopService)
+    // - nicht das drumherum
+    ProductRepository productRepository = mock(ProductRepository.class);
+    OrderRepository orderRepository = mock(OrderRepository.class);
+
+    ShopService shopService = new ShopService(productRepository, orderRepository);
 
     @Test
     void getProductById_whenExistingId_thenReturnProduct() {
         //GIVEN
+        String expectedProductId = "1";
+        Product expectedProduct = new Product(expectedProductId, "Rotwein");
+
+        // Wie soll sich das "gemockte" Repository verhalten?
+        // Der Dummy soll das expectedProduct liefern, wenn ein Aufruf von getProductById
+        // mit der ID expectedProductId kommt
+        when(productRepository.getProductById(expectedProductId)).thenReturn(expectedProduct);
 
         //WHEN
+        Product actualProduct = shopService.getProductById(expectedProductId);
 
         //THEN
+        assertEquals(expectedProduct, actualProduct);
+        // Sicherstellen, dass getProductById auch WIRKLICH aufgerufen wurde
+        verify(productRepository).getProductById(any());
     }
 
     @Test
